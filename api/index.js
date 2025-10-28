1f6c443f1b76faf948bec5dfdd1accbc14ff0b63
Nurbol876 <nurbolsagynbekov876@gmail.com>
2025-10-28 14:12:49 +0600

    cors

--- api/index.js
+++ api/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
 const PORT = 3000;
+const cors = require("cors");
 
 app.use(express.json());
+app.use(cors())
 
 app.get("/api/hello", (req, res) => {
     res.json({ message: "Hello from my API!" });
